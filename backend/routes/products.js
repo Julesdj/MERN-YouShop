c3a8a8910c2d4cc9c426e1bc5963ac9278b2e28a
Kokutse DJOGUENOU <kokutse@Kokutses-MacBook-Pro.local>
2022-06-06 21:00:23 -0400

    CRUD Functionality on products completed

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -11,4 +11,62 @@ products.get('/', async (req, res) => {
     res.send(products);
 });
 
+//Create a new product
+products.post('/', async (req, res) => {
+    //ToDo: Data validation
+    let product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        price: req.body.price,
+        qtyInStock: req.body.qtyInStock,
+        description: req.body.description,
+        tags: req.body.tags,
+    });
+
+    product = await product.save();
+    res.send(product);
+});
+
+//Get single product
+products.get('/:id', async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    //If not existing, return 404
+    if (!product)
+        res.status(404).send('The product with the given Id was not found');
+    res.send(product);
+});
+
+//Update a product
+products.put('/:id', async (req, res) => {
+    //look up the product
+    const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            image: req.body.image,
+            price: req.body.price,
+            qtyInStock: req.body.qtyInStock,
+            description: req.body.description,
+            tags: req.body.tags,
+        },
+        { new: true } //The new attribute is used to get the updated product from the db
+    );
+
+    //If not existing, return 404
+    if (!product)
+        res.status(404).send('The product with the given Id was not found');
+    res.send(product);
+});
+
+//Delete a product
+products.delete('/:id', async (req, res) => {
+    const product = await Product.findByIdAndDelete(req.params.id);
+
+    //If not existing, return 404
+    if (!product)
+        res.status(404).send('The product with the given Id was not found');
+    res.send('Product deleted successfully');
+});
+
 export default products;
