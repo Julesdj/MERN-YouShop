e8402c04a4ebd0d4e188fd3e1cd3a6915da339ba
Kokutse DJOGUENOU <kokutse@Kokutses-MacBook-Pro.local>
2022-06-06 20:19:52 -0400

    MongoDB connection initiated, Module type updated to ES6 Module

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,23 @@
-require('dotenv').config();
-const Express = require('express');
-const app = Express();
+//Dependencies
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import Express from 'express';
+dotenv.config();
 
+//Connection to MongoDB
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log('Connected to YouShop Mongo Database ...'))
+    .catch((err) => console.error('Could not connect to MongoDB ...'));
+
+//Variables
+const app = Express();
+const port = process.env.PORT || 3005;
+
+//Routes
 app.get('/api', (req, res) => {
     res.send('You have reached YouShop api');
 });
-
-const port = process.env.PORT || 3005;
 
 app.listen(port, () => {
     console.log(`The server is running on port ${port}`);
