57554065966477943cdb53cfc212c3a596df869b
Pandaphone <shawngreene075@gmail.com>
2022-06-11 14:20:58 -0400

    Worked on footer elements.

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,6 +2,29 @@
 import React from 'react';
 
 function Footer() {
-    return <div></div>;
+    return <div>
+        <h4>Information</h4>
+        <li style={{listStyleType: 'none'}}>
+            <ul>FAQ</ul>
+            <ul>Returns</ul>
+            <ul>Shipping and Payment</ul>
+            <ul>Terms and Conditions</ul>
+        </li>
+
+        <h4>YouShop</h4>
+        <li style={{listStyleType: 'none'}}>
+            <ul>About Us</ul>
+            <ul>Our Creators</ul>
+            <ul>Feedback</ul>
+        </li>
+
+        <h4>Contact Us</h4>
+        <li style={{listStyleType: 'none'}}>
+            <ul>Email:</ul>
+            <ul>Phone Number:</ul>
+        </li>
+        <h4>Newsletter</h4>
+
+    </div>;
 }
 export default Footer;
