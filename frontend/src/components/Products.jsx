2050c9a63afbda850c70a7d5286f1823b798668b
CKline22 <CKline.1022@gmail.com>
2022-06-10 23:57:00 -0400

    Product page started

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
 
 function Products() {
-    return <div>Products</div>;
+    return(
+        <div>
+            <Container>
+                <Row>
+                    <Col>
+                    <Card border="dark">
+                        <Card.Img variant="top" src="holder.js/100px180"/>
+                        <Card.Body>
+                            <Card.Title>Product</Card.Title>
+                            <Card.Text>About This Product</Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                    <Col>
+                    <Card border="dark">
+                        <Card.Img variant="top" src="holder.js/100px180"/>
+                        <Card.Body>
+                            <Card.Title>Product</Card.Title>
+                            <Card.Text>About This Product</Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                    <Card border="dark">
+                        <Card.Img variant="top" src="holder.js/100px180"/>
+                        <Card.Body>
+                            <Card.Title>Product</Card.Title>
+                            <Card.Text>About This Product</Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                    <Col>
+                    <Card border="dark">
+                        <Card.Img variant="top" src="holder.js/100px180"/>
+                        <Card.Body>
+                            <Card.Title>Product</Card.Title>
+                            <Card.Text>About This Product</Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
 }
 
 export default Products;
