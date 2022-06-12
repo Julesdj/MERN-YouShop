621a96457b7e33f2f4f94de26c45a1978bd2f26a
Pandaphone <shawngreene075@gmail.com>
2022-06-12 06:10:22 -0400

    Edited the ReadMe and Footer a bit.

--- README.md
+++ README.md
@@ -16,3 +16,9 @@ N-Node
 The implementation of some of the style elements used in our Figma mockup. Shawn Greene did a good amount of the Figma part, though some features couldn't be put in due to constraints.
 
 In the future, when submitting something like this to employers we'd like to go back and add some of the features that would really differentiate this from other online E-Commerce websites in the space. Initial ideas proposed at the beginning of the project included things such as having an interest quiz available and tags on every product and storefront which took from that quiz's answers.
+
+# Getting Started
+
+In order to use this project on your own computer you first need to fork from the repository <a href='https://github.com/Julesdj/MERN-YouShop.git'>here.</a>
+
+From there enter your terminal, cd into whichever directory holds this project (simply typing the start of the directory name and pressing tab will autocomplete for you), and cd into the project folder. Use 'npm install' to install the required packages and 'npm start' to start the project in the default browser.
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,29 +2,38 @@
 import React from 'react';
 
 function Footer() {
-    return <div>
+    return <div className="main-footer">
+        <div className="container"> 
+
+        <div className='row'>
         <h4>Information</h4>
-        <li style={{listStyleType: 'none'}}>
-            <ul>FAQ</ul>
-            <ul>Returns</ul>
-            <ul>Shipping and Payment</ul>
-            <ul>Terms and Conditions</ul>
-        </li>
+        <ul style={{listStyleType: 'none'}}>
+            <li>FAQ</li>
+            <li>Returns</li>
+            <li>Shipping and Payment</li>
+            <li>Terms and Conditions</li>
+        </ul></div>
 
-        <h4>YouShop</h4>
-        <li style={{listStyleType: 'none'}}>
-            <ul>About Us</ul>
-            <ul>Our Creators</ul>
-            <ul>Feedback</ul>
-        </li>
-
-        <h4>Contact Us</h4>
-        <li style={{listStyleType: 'none'}}>
-            <ul>Email:</ul>
-            <ul>Phone Number:</ul>
-        </li>
-        <h4>Newsletter</h4>
-
+        <div className='row'>
+            <h4>YouShop</h4>
+        <ul style={{listStyleType: 'none'}}>
+            <li>About Us</li>
+            <li>Our Creators</li>
+            <li>Feedback</li>
+        </ul></div>
+        
+        <div className='row'>
+            <h4>Contact Us</h4>
+        <ul style={{listStyleType: 'none'}}>
+            <li>Email:</li>
+            <li>Phone Number:</li>
+        </ul></div>
+        
+        <div className='row'></div>
+        <h4>Socials</h4></div>
+        
     </div>;
 }
 export default Footer;
+
+
