381666cb6dd999436820f6a85f457342f5ef1e51
Pandaphone <shawngreene075@gmail.com>
2022-06-19 19:13:36 -0400

    Think it works this time???

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,7 +2,7 @@
 import React from 'react';
 
 function Footer() {
-    return <div className="main-footer" fixed='bottom' width='100%'>
+    return <div className="main-footer" style={{width: '100%', position: 'fixed', left: '0', bottom: '0'}}>
         <div className="container" style={{display: 'flex', backgroundColor: 'black', color: 'white'}}> 
 
         <div className='row'>
