9454be6e6faa80aa1c2d3202166d4c97da5bc27b
Pandaphone <shawngreene075@gmail.com>
2022-06-20 16:35:35 -0400

    Converted Contact from a card. Was too frustrating to work with.

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,42 +1,40 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card'
-import CardGroup from 'react-bootstrap/CardGroup'
 
 function Contact() {
     return(
         <div>
-            <Card>
-                <Card.Img variant="top" src="holder.js/100px180"/>
-                <Card.Body style={{ textAlign: 'center' }}>
-                    <Card.Title>Contact Us</Card.Title>
-                    <Card.Text>Contact YouShop</Card.Text>
-                </Card.Body>
-                <CardGroup>
-                    <Card border='dark'>
-                        <Card.Body>
-                            <Card.Title>Phone Numbers</Card.Title>
-                            <Card.Text>(123)-456-7899 YouShop Office</Card.Text>
-                            <Card.Text>(123)-456-7898 YouShop Customer Service</Card.Text>
-                            <Card.Text>(123)-456-7897 YouShop Returns and Order Info</Card.Text>
-                        </Card.Body>
-                    </Card>
-                    <Card border='dark'>
-                        <Card.Body>
-                            <Card.Title>Address</Card.Title>
-                            <Card.Text>123 River rd. </Card.Text>
-                            <Card.Text>Come tour our warehouse</Card.Text>
-                        </Card.Body>
-                    </Card>
-                    <Card border='dark'>
-                        <Card.Body>
-                            <Card.Title>Emails and Customer Service</Card.Title>
-                            <Card.Text>YouShop@gmail.com</Card.Text>
-                            <Card.Text>YouShopService@email.com</Card.Text>
-                            <Card.Text>YouShopOrderInfo@email.com</Card.Text>
-                        </Card.Body>
-                    </Card>
-                </CardGroup>
-            </Card>
+            <div>
+
+                <div style={{ textAlign: 'center' }}>
+                    <div>
+                        Contact Us
+                        <br></br>
+                        Contact YouShop
+                    </div>
+                </div>
+
+                <div>
+                    Phone Numbers
+                    (123)-456-7899 YouShop Office
+                    (123)-456-7898 YouShop Customer Service
+                    (123)-456-7897 YouShop Returns and Order Info
+                </div>
+                    
+                    
+                <div>
+                    Address
+                    123 River rd.
+                    Come tour our warehouse
+                </div>
+
+                <div>
+                     Emails and Customer Service
+                    YouShop@gmail.com
+                    YouShopService@email.com
+                    YouShopOrderInfo@email.com
+                </div>
+                
+            </div>
         </div>
     )
 }
