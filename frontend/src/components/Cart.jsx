db0f60074ac8ba867a0901cceaa4093545451ef7
Pandaphone <shawngreene075@gmail.com>
2022-06-19 20:25:11 -0400

    Working with the cart. Gonna look weird

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
 
 function Cart() {
-    return <div>Cart</div>;
+    return <div>
+    <Table responsive>
+        <thead>
+            <tr>
+                <th></th>
+                <th>Title</th>
+                <th>Status</th>
+                <th>Inventory</th>
+                <th>Update</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            {products.map((product) => (
+                <tr key={product._id}>
+                    <td>
+                        <Form.Check type="checkbox" />
+                    </td>
+
+                    <td>{product.name}</td>
+                    <td>Status</td>
+                    <td>{product.qtyInStock}</td>
+                    {/* TODO: Add edit functionality */}
+                    <td>
+                        <Link to={'/store/admin/products/edit'}>
+                            Edit
+                        </Link>
+                    </td>
+                    <td>
+                        <MdDelete
+                            onClick={(e) =>
+                                setProductId(`${product._id}`)
+                            }
+                            style={{ fontSize: '30px' }}
+                            variant="warning"
+                        />
+                    </td>
+                </tr>
+            ))}
+        </tbody>
+    </Table>
+</div>
 }
 
 export default Cart;
