66164dbdce8a349f99192523a05a1d9ee423c5e9
Pandaphone <shawngreene075@gmail.com>
2022-06-20 17:18:49 -0400

    Testing once more.

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -3,37 +3,7 @@ import React from 'react';
 function Contact() {
     return(
         <div>
-            <div>
-            <div style={{ textAlign: 'center' }}>
-                    <div>
-                        Contact Us
-                        <br></br>
-                        Contact YouShop
-                    </div>
-                
-
-                 <div>
-                    <h2>Phone Numbers</h2>
-                    (123)-456-7899 YouShop Office
-                    (123)-456-7898 YouShop Customer Service
-                    (123)-456-7897 YouShop Returns and Order Info
-                </div>
-                    
-                    
-                <div>
-                    Address
-                    123 River rd.
-                    Come tour our warehouse
-                </div>
-
-                <div>
-                     Emails and Customer Service
-                    YouShop@gmail.com
-                    YouShopService@email.com
-                    YouShopOrderInfo@email.com
-                </div>
-            </div>
-            </div>
+            Contact!?
         </div>
     )
 }
