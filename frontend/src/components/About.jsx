1b12dd24427430c301508e68f6b41657d1a9762f
Pandaphone <shawngreene075@gmail.com>
2022-06-20 16:23:52 -0400

    More redundant code removed.

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -4,14 +4,19 @@ function About() {
     return (
         <div style={{ display: 'flex', justifyContent: 'center', backgroundColor: 'transparent'}}>
                 <div style={{textAlign: 'center'}}>
-                    <div>About YouShop</div>
-                    <div>Your Favorite Online Store</div>
+                    <div>
+                        About YouShop
+                        <br></br>   
+                        Your Favorite Online Store
+                    </div>
+
                     <div style={{fontSize: '40px', color: 'white'}}>Welcome to YouShop. We’re glad you’re here.</div>
                     <p style={{width: "85rem", margin: 'auto'}}>
-                        YouShop was officially created in 2022 but the idea existed long  before that. It's creators Kokutse Djoguenou, Connor Kline, and Shawn Greene had conversed about the idea of a shop that suits one's needs for quite some time. Amazon did this somewhat well, but it didn't really 'know' it's customer. No E-Commerce platform did, as far as we knew. So we set out to change that and the result was Youshop. 
+                        YouShop was officially created in 2022 but the idea existed long  before that. It's creators Kokutse Djoguenou, Connor Kline, and Shawn Greene had conversed about the idea of a shop that suits one's needs for quite some time. Amazon did this somewhat well, but it didn't really 'know' it's customer. No E-Commerce platform did, as far as they knew. So they set out to change that and the result was YouShop. 
                     
                         <br></br>   
-                        Our goal is to be personal; to help YOU, whoever you are. That's why YouShop asks about what you love. That's why Youshop 
+
+                        Our goal is to be personal; to help YOU, whoever you are. That's why YouShop asks about what you love. That's why YouShop 
                         donates 30% of it's yearly profits to various charities.
 
                         <br></br>
